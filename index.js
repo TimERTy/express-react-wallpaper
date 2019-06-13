d22c0090ae1f9f8592c7612274909076bfaa8825
TimERTy <tmahonert@gmail.com>
2019-06-13 16:42:13 +1000

    changed port and added deploy script

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 55555;
 app.listen(port);
 
 console.log("App is listening on port " + port);
